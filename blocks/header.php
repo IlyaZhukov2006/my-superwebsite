3c0eee427a8ccf233dfc042c2a69f2757e4215c0
ilya <dfbgdfh@mail.ru>
2025-06-17 15:33:36 +0300

    Заменил html на php и исправил index.html

---
+++ blocks/header.php
@@ -0,0 +1,7 @@
+<table>
+            <tr>
+                <td><a href="website.php">Главная</a></td>
+                <td><a href="about.php">Дискография</a></td>
+                <td><a href="contact.php">Обратная связь</a></td>
+            </tr>
+        </table>
